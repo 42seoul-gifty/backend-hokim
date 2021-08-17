8c21908968472b42a37c4cfc75225decbb1319c6
hokim <teamgoodaloe@gmail.com>
2021-08-18 00:48:41 +0900

    fix: add kakao login

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const db = require("./models");
 
 class App {
   constructor() {
     this.app = express();
-
+    this.app.use(express.json());
     this.app.use(express.static("./public"));
     this.app.use(require("./routes"));
     this.app.set("view engine", "ejs");
+
+    this.dbConnection();
+  }
+
+  dbConnection() {
+    // DB authentication
+    db.sequelize
+      .authenticate()
+      .then(() => {
+        console.log("Connection has been established successfully.");
+        return db.sequelize.sync();
+      })
+      .then(() => {
+        console.log("DB Sync complete.");
+      })
+      .catch((err) => {
+        console.error("Unable to connect to the database:", err);
+      });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../controller/auth");
 
 router.get("/", (req, res) => {
   res.render("index", {});
 });
 
+router.use("/login", auth);
 module.exports = router;
